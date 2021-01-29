c2067468ace32648c73c906fe1bd63d4ace37f8d
cabrerojas <gcabrerojas@gmail.com>
2021-01-28 21:36:52 -0300

    page por pais

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +11,7 @@ import { AppRoutingModule } from '../app-routing.module';
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    RouterModule
   ],
   exports:[
     SidebarComponent
